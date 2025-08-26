2d503050b8d2a065480c38edd35fc631c822e49f
Jansen Alexandre <jansen3lenxadre@gmail.com>
2025-08-26 18:31:27 -0300

    Terminado a Class Criptografia

---
+++ Projeto RH-HOTELARIA/Data/IRepository/IRH_DocumentoEndereco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_RH_HOTELARIA.Data.IRepository
+{
+    public interface IRH_DocumentoEndereco
+    {
+    }
+}
--- Projeto RH-HOTELARIA/Utils/Criptografia.cs
+++ Projeto RH-HOTELARIA/Utils/Criptografia.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BCrypt.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,32 @@ namespace Projeto_RH_HOTELARIA.Utils
 {
     class Criptografia
     {
+        public static string HashSenha(string senha)
+        {
+            string hash = null;
+            if (Validacoes.SenhaValida(senha) == true)
+            {
+                hash = BCrypt.Net.BCrypt.HashPassword(senha);
+                return hash;
+            }
+            else
+            {
+                throw new Exception("A senha não é valida!");
+            }
+            
+        }
+
+        public static bool Verificar(string senha, string hashDB)
+        {
+            bool verify = BCrypt.Net.BCrypt.Verify(senha, hashDB);
+            if(verify == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
