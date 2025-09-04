c5186e31500f24a6c939e9c973f9b279c463510d
Jansen Alexandre <jansen3lenxadre@gmail.com>
2025-09-04 16:32:42 -0300

    Finalizado o Repository

--- Projeto RH-HOTELARIA/Data/IRepository/ISYS_Usuario.cs
+++ Projeto RH-HOTELARIA/Data/IRepository/ISYS_Usuario.cs
@@ -6,7 +6,7 @@ namespace Projeto_RH_HOTELARIA.Data.IRepository
     public interface ISYS_Usuario
     {
         void Inserir(SYS_Usuario usuario);
-        SYS_Usuario BuscarPorLogin(string login);
+        List<SYS_Usuario> BuscarPorLogin(string login);
         List<SYS_Usuario> ListarTodos();
         void Alterar(SYS_Usuario usuario);
         void Excluir(int id);
--- Projeto RH-HOTELARIA/Data/Repository/RH_FuncionarioRepository.cs
+++ Projeto RH-HOTELARIA/Data/Repository/RH_FuncionarioRepository.cs
@@ -5,10 +5,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Configuration;
 
 namespace Projeto_RH_HOTELARIA.Data.Repository
 {
